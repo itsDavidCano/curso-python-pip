593dec4b85740bf297d8a37f50dc858cf18873b1
itsDavidCano <davidcano79@gmail.com>
2024-03-23 12:15:21 -0500

    add docs

---
+++ app/example.py
@@ -0,0 +1,4 @@
+import main 
+print(main.data)
+#app/main.py esta corriendo de este lado
+main.run()
---
+++ charts/main.py
@@ -0,0 +1,8 @@
+import charts
+
+def run():
+    charts.generate_pie_chart()
+
+#Ejecutarlo como un script
+if __name__ == '__main__':
+    run()
